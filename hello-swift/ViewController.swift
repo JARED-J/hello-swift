b95e4b2bf1beb28b83219c9cb9b989464adcc461
Jared E Jackson <jaredjxn15@gmail.com>
2018-06-13 14:02:59 -0400

    Initial Commit

---
+++ hello-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-swift
+//
+//  Created by Jared  Jackson on 6/13/18.
+//  Copyright © 2018 Jared  Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
